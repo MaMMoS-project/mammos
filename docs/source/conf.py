5c6005be4ad4720849c3cf4bfb7c52b417ce4762
Martin Lang <martin.lang@mpsd.mpg.de>
2025-06-11 17:51:26 +0200

    Sphinx: enable nitpicky and fail on warning

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -7,7 +7,10 @@ from __future__ import annotations
 from typing import TYPE_CHECKING
 from sphinx.domains.python import PythonDomain
 
+from sphinx.ext import intersphinx
+
 if TYPE_CHECKING:
+    from sphinx import addnodes, application, environment
     from docutils.nodes import Element
     from sphinx.application import Sphinx
     from sphinx.builders import Builder
@@ -22,6 +25,11 @@ author = "Thomas Schrefl, Swapneel Amit Pathak, Andrea Petrocchi, Samuel Holt, M
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+nitpicky = True
+nitpick_ignore = [
+    ('py:class', 'owlready2.entity.ThingClass'),
+]
 
 extensions = [
     "nbsphinx",
@@ -54,6 +62,7 @@ intersphinx_mapping = {
     "astropy": ("https://docs.astropy.org/en/stable", None),
     "matplotlib": ("https://matplotlib.org/stable", None),
     "numpy": ("https://numpy.org/doc/stable/", None),
+    "owlready2": ("https://owlready2.readthedocs.io/en/latest/", None),
     "pandas": ("https://pandas.pydata.org/docs/", None),
     "pydantic": ("https://docs.pydantic.dev/latest", None),
     "python": ("https://docs.python.org/3", None),
@@ -89,28 +98,56 @@ html_sidebars = {
 }
 
 
-# code snippet for fixing mappings taken from
-# https://github.com/tox-dev/pyproject-api/blob/136e5ded8f65fb157c2e5fee5e8e05de9eefcdd4/docs/conf.py
-def setup(app: Sphinx) -> None:  # noqa: D103
-    class PatchedPythonDomain(PythonDomain):
-        def resolve_xref(  # noqa: PLR0913,PLR0917
-            self,
-            env: BuildEnvironment,
-            fromdocname: str,
-            builder: Builder,
-            type: str,  # noqa: A002
-            target: str,
-            node: resolve_xref,
-            contnode: Element,
-        ) -> Element:
-            # fixup some wrongly resolved mappings
-            mapping = {
-                "pathlib._local.Path": "pathlib.Path",
-            }
-            if target in mapping:
-                target = node["reftarget"] = mapping[target]
-            return super().resolve_xref(
-                env, fromdocname, builder, type, target, node, contnode
-            )
+# -- Code to fix various Sphinx issues related to type hints -----------------
 
+# code snippet for fixing mapping for pathlib.Path, taken from
+# https://github.com/tox-dev/pyproject-api/blob/136e5ded8f65fb157c2e5fee5e8e05de9eefcdd4/docs/conf.py
+class PatchedPythonDomain(PythonDomain):
+    def resolve_xref(  # noqa: PLR0913,PLR0917
+        self,
+        env: BuildEnvironment,
+        fromdocname: str,
+        builder: Builder,
+        type: str,  # noqa: A002
+        target: str,
+        node: resolve_xref,
+        contnode: Element,
+    ) -> Element:
+        # fixup some wrongly resolved mappings
+        mapping = {
+            "pathlib._local.Path": "pathlib.Path",
+        }
+        if target in mapping:
+            target = node["reftarget"] = mapping[target]
+        return super().resolve_xref(
+            env, fromdocname, builder, type, target, node, contnode
+        )
+
+
+# A custom handler for :py:data:`numpy.typing.ArrayLike`
+# Based on https://github.com/sphinx-doc/sphinx/issues/13308
+def missing_reference_handler(
+    app: application.Sphinx,
+    env: environment.BuildEnvironment,
+    node: addnodes.pending_xref,
+    contnode,
+):
+    target = node["reftarget"]
+    if "." in target and node["reftype"] == "class":
+        # Try again as `obj` so we pick up Unions, TypeVars and other things
+        if target.startswith("ophyd_async"):
+            # Pick it up from our domain
+            domain = env.domains[node["refdomain"]]
+            refdoc = node.get("refdoc")
+            return domain.resolve_xref(
+                env, refdoc, app.builder, "data", target, node, contnode
+            )
+        else:
+            # pass it to intersphinx with the right type
+            node["reftype"] = "data"
+            return intersphinx.missing_reference(app, env, node, contnode)
+
+
+def setup(app: application.Sphinx):
+    app.connect("missing-reference", missing_reference_handler)
     app.add_domain(PatchedPythonDomain, override=True)
