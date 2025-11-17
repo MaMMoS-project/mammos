7fb70b6f9b17ae835bdba6e7c47ad3b358936dc9
Martin Lang <67915889+lang-m@users.noreply.github.com>
2025-11-17 10:58:29 +0100

    Switch from nbsphinx to myst-nb (#43)
    * Switch from nbsphinx to myst-nb: Avoids rendering issues with nested markdown.
    
    * Custom CSS to get cell numbering
    
    * Change style of cell numbering
    
    Closes MaMMoS-project/mammos-entity#73.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -32,7 +32,7 @@ nitpick_ignore = [
 ]
 
 extensions = [
-    "nbsphinx",
+    "myst_nb",
     "sphinx_copybutton",
     "sphinx_design",
     "sphinx.ext.autodoc",
@@ -77,6 +77,7 @@ numfig_format = {
     'table': 'Table %s',
     'code-block': 'Listing %s',
 }
+nb_execution_mode = "off"
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
